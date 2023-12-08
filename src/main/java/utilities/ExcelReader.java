9e8618fd7ae77a82c5dd64a886d6626913c88db7
Prashanth <prashanthsamudrala.nspt@gmail.com>
2023-12-08 23:07:49 +0530

    Additional comments cloud to cloud execution

--- src/main/java/utilities/ExcelReader.java
+++ src/main/java/utilities/ExcelReader.java
@@ -1,55 +1,153 @@
 package utilities;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
-import org.apache.poi.xssf.usermodel.XSSFRow;
-import org.apache.poi.xssf.usermodel.XSSFSheet;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import org.apache.poi.ss.util.NumberToTextConverter;
 
 public class ExcelReader {
 
+	public List<Map<String, String>> getData(String excelFilePath, String sheetName)
+			throws InvalidFormatException, IOException {
+		Sheet sheet = getSheetByName(excelFilePath, sheetName);
+		return readSheet(sheet);
+	}
 
-    public static void main(String[] args) throws IOException {
+	public List<Map<String, String>> getData(String excelFilePath, int sheetNumber)
+			throws InvalidFormatException, IOException {
+		Sheet sheet = getSheetByIndex(excelFilePath, sheetNumber);
+		return readSheet(sheet);
+	}
 
-        // read the file path
-        FileInputStream input =new FileInputStream
-                (System.getProperty("user.dir")+"/src/test/resources/testData/TestDataSample.xlsx");
+	private Sheet getSheetByName(String excelFilePath, String sheetName) throws IOException, InvalidFormatException {
+		Sheet sheet = getWorkBook(excelFilePath).getSheet(sheetName);
+		return sheet;
+	}
 
+	private Sheet getSheetByIndex(String excelFilePath, int sheetNumber) throws IOException, InvalidFormatException {
+		Sheet sheet = getWorkBook(excelFilePath).getSheetAt(sheetNumber);
+		return sheet;
+	}
 
-        // supply file path to workbook as a reference
-        XSSFWorkbook workbook= new XSSFWorkbook(input);
+	private Workbook getWorkBook(String excelFilePath) throws IOException, InvalidFormatException {
+		return WorkbookFactory.create(new File(excelFilePath));
+	}
 
-        // read sheet
+	private List<Map<String, String>> readSheet(Sheet sheet) {
+		Row row;
+		int totalRow = sheet.getPhysicalNumberOfRows();
+		List<Map<String, String>> excelRows = new ArrayList<Map<String, String>>();
+		int headerRowNumber = getHeaderRowNumber(sheet);
+		if (headerRowNumber != -1) {
+			int totalColumn = sheet.getRow(headerRowNumber).getLastCellNum();
+			int setCurrentRow = 1;
+			for (int currentRow = setCurrentRow; currentRow <= totalRow; currentRow++) {
+				row = getRow(sheet, sheet.getFirstRowNum() + currentRow);
+				LinkedHashMap<String, String> columnMapdata = new LinkedHashMap<String, String>();
+				for (int currentColumn = 0; currentColumn < totalColumn; currentColumn++) {
+					columnMapdata.putAll(getCellValue(sheet, row, currentColumn));
+				}
+				excelRows.add(columnMapdata);
+			}
+		}
+		return excelRows;
+	}
 
-        XSSFSheet sheet=workbook.getSheetAt(0);
+	private int getHeaderRowNumber(Sheet sheet) {
+		Row row;
+		int totalRow = sheet.getLastRowNum();
+		for (int currentRow = 0; currentRow <= totalRow + 1; currentRow++) {
+			row = getRow(sheet, currentRow);
+			if (row != null) {
+				int totalColumn = row.getLastCellNum();
+				for (int currentColumn = 0; currentColumn < totalColumn; currentColumn++) {
+					Cell cell;
+					cell = row.getCell(currentColumn);
+					if (cell.getCellType() == Cell.CELL_TYPE_STRING) {
+						return row.getRowNum();
 
-        XSSFRow row=sheet.getRow(0);
+					} else if (cell.getCellType() == Cell.CELL_TYPE_NUMERIC) {
+						return row.getRowNum();
 
-        System.out.println("Number of rows in excel data::"+sheet.getPhysicalNumberOfRows());
-        System.out.println("Number of columns::"+row.getPhysicalNumberOfCells());
+					} else if (cell.getCellType() == Cell.CELL_TYPE_BOOLEAN) {
+						return row.getRowNum();
+					} else if (cell.getCellType() == Cell.CELL_TYPE_FORMULA) {
+						return row.getRowNum();
+					}
+				}
+			}
+		}
+		return (-1);
+	}
 
+	private Row getRow(Sheet sheet, int rowNumber) {
+		return sheet.getRow(rowNumber);
+	}
 
-        for(int i=0; i< sheet.getPhysicalNumberOfRows(); i++ ){
-
-            for(int j=0; j< row.getPhysicalNumberOfCells(); j++){
-
-                System.out.println(sheet.getRow(i).getCell(j).getStringCellValue());
-
-            }
-
-        }
-
-
-
-
-
-    }
-
-
+	private LinkedHashMap<String, String> getCellValue(Sheet sheet, Row row, int currentColumn) {
+		LinkedHashMap<String, String> columnMapdata = new LinkedHashMap<String, String>();
+		Cell cell;
+		if (row == null) {
+			if (sheet.getRow(sheet.getFirstRowNum()).getCell(currentColumn)
+					.getCellType() != Cell.CELL_TYPE_BLANK) {
+				String columnHeaderName = sheet.getRow(sheet.getFirstRowNum()).getCell(currentColumn)
+						.getStringCellValue();
+				columnMapdata.put(columnHeaderName, "");
+			}
+		} else {
+			cell = row.getCell(currentColumn);
+			if (cell.getCellType() == Cell.CELL_TYPE_STRING) {
+				if (sheet.getRow(sheet.getFirstRowNum())
+						.getCell(cell.getColumnIndex())
+						.getCellType() != Cell.CELL_TYPE_BLANK) {
+					String columnHeaderName = sheet.getRow(sheet.getFirstRowNum()).getCell(cell.getColumnIndex())
+							.getStringCellValue();
+					columnMapdata.put(columnHeaderName, cell.getStringCellValue());
+				}
+			} else if (cell.getCellType() == Cell.CELL_TYPE_NUMERIC) {
+				if (sheet.getRow(sheet.getFirstRowNum())
+						.getCell(cell.getColumnIndex())
+						.getCellType() != Cell.CELL_TYPE_BLANK) {
+					String columnHeaderName = sheet.getRow(sheet.getFirstRowNum()).getCell(cell.getColumnIndex())
+							.getStringCellValue();
+					columnMapdata.put(columnHeaderName, NumberToTextConverter.toText(cell.getNumericCellValue()));
+				}
+			} else if (cell.getCellType() == Cell.CELL_TYPE_BLANK) {
+				if (sheet.getRow(sheet.getFirstRowNum())
+						.getCell(cell.getColumnIndex())
+						.getCellType() != Cell.CELL_TYPE_BLANK) {
+					String columnHeaderName = sheet.getRow(sheet.getFirstRowNum()).getCell(cell.getColumnIndex())
+							.getStringCellValue();
+					columnMapdata.put(columnHeaderName, "");
+				}
+			} else if (cell.getCellType() == Cell.CELL_TYPE_BOOLEAN) {
+				if (sheet.getRow(sheet.getFirstRowNum())
+						.getCell(cell.getColumnIndex())
+						.getCellType() != Cell.CELL_TYPE_BLANK) {
+					String columnHeaderName = sheet.getRow(sheet.getFirstRowNum()).getCell(cell.getColumnIndex())
+							.getStringCellValue();
+					columnMapdata.put(columnHeaderName, Boolean.toString(cell.getBooleanCellValue()));
+				}
+			} else if (cell.getCellType() == Cell.CELL_TYPE_ERROR) {
+				if (sheet.getRow(sheet.getFirstRowNum())
+						.getCell(cell.getColumnIndex())
+						.getCellType() != Cell.CELL_TYPE_BLANK) {
+					String columnHeaderName = sheet.getRow(sheet.getFirstRowNum()).getCell(cell.getColumnIndex())
+							.getStringCellValue();
+					columnMapdata.put(columnHeaderName, Byte.toString(cell.getErrorCellValue()));
+				}
+			}
+		}
+		return columnMapdata;
+	}
 }
